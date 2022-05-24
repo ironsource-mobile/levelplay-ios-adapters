2ff9a9fe1c56fce122be46e1efcb7a2b824e2763
Alon Dotan <alond@is.com>
2022-05-24 16:34:17 +0300

    no message

--- Adapters/Google (AdMob and Ad Manager)/BuildScript/build_XCFramework.sh
+++ Adapters/Google (AdMob and Ad Manager)/BuildScript/build_XCFramework.sh
@@ -37,7 +37,7 @@ xcrun xcodebuild -workspace "${ADAPTER_WORKSPACE}" \
 
 
   createFramework "iphoneos" "armv7 arm64"
-  createFramework "iphonesimulator" "x86_64 i386" 
+  createFramework "iphonesimulator" "x86_64" 
 
 mkdir ${OUTPUT_FOLDER}
 
