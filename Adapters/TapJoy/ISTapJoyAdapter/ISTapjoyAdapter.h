0063db296a032129ad81d1df1c229db5972ecef1
Alon Dotan <alond@is.com>
2022-07-31 17:22:26 +0300

    TapJoy Adapter 4.1.20 supports SDK 12.10.0

---
+++ Adapters/TapJoy/ISTapJoyAdapter/ISTapjoyAdapter.h
@@ -0,0 +1,28 @@
+//
+//  ISTapjoyAdapter.h
+//  ISTapjoyAdapter
+//
+//  Created by Daniil Bystrov on 4/13/16.
+//  Copyright © 2016 IronSource. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IronSource/ISBaseAdapter+Internal.h"
+
+static NSString * const TapjoyAdapterVersion = @"4.1.20";
+static NSString * GitHash = @"";
+
+//System Frameworks For Tapjoy Adapter
+
+@import AdSupport;
+@import CFNetwork;
+@import CoreServices;
+@import CoreTelephony;
+@import SystemConfiguration;
+@import StoreKit;
+@import UIKit;
+@import WebKit;
+
+@interface ISTapjoyAdapter : ISBaseAdapter
+
+@end
