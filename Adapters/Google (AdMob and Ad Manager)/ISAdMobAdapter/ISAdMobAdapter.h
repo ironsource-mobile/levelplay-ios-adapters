269cfcab0aee21eb9a77e37e12e4a76d4d2ceea7
Hadar Pur <hadar.pur@is.com>
2023-05-28 12:01:54 +0300

    Release AdMob Adapter 10.5.0 supports SDK 4.3.43

--- Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/ISAdMobAdapter.h
+++ Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/ISAdMobAdapter.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
  
-static NSString * const AdMobAdapterVersion = @"4.3.42";
+static NSString * const AdMobAdapterVersion = @"4.3.43";
 static NSString * Githash = @"";
 
 //System Frameworks For AdMob Adapter
