c8616c1eebe2e9c2da3e7ad489ca6393d19389cb
Smira Ibrahim Rassas <samira.ibrahimrassas@is.com>
2024-01-07 15:52:42 +0200

    Release Chartboost Adapter 4.3.17 supports SDK 9.6.0

--- Adapters/Chartboost/ISChartboostAdapter/ISChartboostAdapter.h
+++ Adapters/Chartboost/ISChartboostAdapter/ISChartboostAdapter.h
@@ -9,7 +9,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <IronSource/IronSource.h>
 
-static NSString * const ChartboostAdapterVersion = @"4.3.16";
+static NSString * const ChartboostAdapterVersion = @"4.3.17";
 static NSString * Githash = @"";
 
 //System Frameworks For Chartboost Adapter
