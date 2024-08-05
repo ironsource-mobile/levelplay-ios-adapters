aabbf21823c16a171b63248671bb913bbed3da32
Smira Ibrahim Rassas <samira.ibrahimrassas@is.com>
2024-08-05 17:30:43 +0300

    Release InMobi Adapter 4.3.24 supports SDK 10.7.5

--- Adapters/InMobi/ISInMobiAdapter/ISInMobiAdapter.h
+++ Adapters/InMobi/ISInMobiAdapter/ISInMobiAdapter.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const InMobiAdapterVersion = @"4.3.23";
+static NSString * const InMobiAdapterVersion = @"4.3.24";
 static NSString * Githash = @"";
  
 //System Frameworks For InMobi Adapter
