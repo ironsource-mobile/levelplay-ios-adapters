1198f5cc5e0274d9deef1b4b566e1c69bfd8aec2
Smira Ibrahim Rassas <samira.ibrahimrassas@unity3d.com>
2024-12-08 18:37:53 +0200

    Release MobileFuse Adapter 4.3.3 supports SDK 1.8.2

--- Adapters/MobileFuse/ISMobileFuseAdapter/ISMobileFuseAdapter.h
+++ Adapters/MobileFuse/ISMobileFuseAdapter/ISMobileFuseAdapter.h
@@ -7,7 +7,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const mobileFuseAdapterVersion = @"4.3.2";
+static NSString * const mobileFuseAdapterVersion = @"4.3.3";
 static NSString * Githash = @"";
 
 //No System Frameworks For MobileFuse Adapter are required
