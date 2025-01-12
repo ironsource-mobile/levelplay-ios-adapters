3dcc4cee27be78b45e243032d5b017e3667e0cb2
Smira Ibrahim Rassas <samira.ibrahimrassas@unity3d.com>
2025-01-12 19:09:46 +0200

    Release Pangle Adapter 4.3.38 supports SDK 6.4.1.1

--- Adapters/Pangle/ISPangleAdapter/ISPangleAdapter.h
+++ Adapters/Pangle/ISPangleAdapter/ISPangleAdapter.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const PangleAdapterVersion = @"4.3.35";
+static NSString * const PangleAdapterVersion = @"4.3.38";
 static NSString * Githash = @"";
 
 // System Frameworks For Pangle Adapter
