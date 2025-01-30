ffeaa82a40a4f5b919d8183b246316fbb58d1233
Smira Ibrahim Rassas <samira.ibrahimrassas@unity3d.com>
2025-01-30 16:26:52 +0200

    Release Yandex Adapter 4.3.9 supports SDK 7.9.0

--- Adapters/Yandex/ISYandexAdapter/ISYandexAdapter.h
+++ Adapters/Yandex/ISYandexAdapter/ISYandexAdapter.h
@@ -8,7 +8,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <IronSource/IronSource.h>
 
-static NSString * const YandexAdapterVersion = @"4.3.8";
+static NSString * const YandexAdapterVersion = @"4.3.9";
 static NSString * Githash = @"";
 
 //System Frameworks For Yandex Adapter
