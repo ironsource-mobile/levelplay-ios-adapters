a77ba0da9786aff54e452389dead438604243ae6
Smira Ibrahim Rassas <samira.ibrahimrassas@unity3d.com>
2025-02-27 16:23:14 +0200

    Release AdMob Adapter 4.3.62 supports SDK 12.1.0

--- Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/ISAdMobAdapter.h
+++ Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/ISAdMobAdapter.h
@@ -9,7 +9,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
-static NSString * const AdMobAdapterVersion = @"4.3.61";
+static NSString * const AdMobAdapterVersion = @"4.3.62";
 static NSString * Githash = @"";
 
 //System Frameworks For AdMob Adapter
