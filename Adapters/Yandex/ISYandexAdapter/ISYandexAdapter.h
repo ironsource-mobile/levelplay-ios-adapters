e97fd1c4227d5f66344d59022cbc6eb3dc5e8bae
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-04-06 08:41:26 +0000

    Release Yandex Adapter 4.3.15 Supports SDK 7.12.0

--- Adapters/Yandex/ISYandexAdapter/ISYandexAdapter.h
+++ Adapters/Yandex/ISYandexAdapter/ISYandexAdapter.h
@@ -8,7 +8,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <IronSource/IronSource.h>
 
-static NSString * const YandexAdapterVersion = @"4.3.14";
+static NSString * const YandexAdapterVersion = @"4.3.15";
 static NSString * Githash = @"";
 
 //System Frameworks For Yandex Adapter
