525b3494adcc2e8eb6ae3dfa07fdf95f66d34d14
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-04-27 12:12:16 +0000

    Release Vungle Adapter 4.3.44 Supports SDK 7.5.0

--- Adapters/Vungle/ISVungleAdapter/ISVungleAdapter.h
+++ Adapters/Vungle/ISVungleAdapter/ISVungleAdapter.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const VungleAdapterVersion = @"4.3.43";
+static NSString * const VungleAdapterVersion = @"4.3.44";
 static NSString * Githash = @"";
 
 //System Frameworks For Vungle Adapter
