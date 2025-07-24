19397004a2f7c0d0b9312fc26a45d30582dff24c
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-07-24 11:46:37 +0000

    Release UnityAds Adapter 4.3.54 Supports SDK 4.16.0

--- Adapters/UnityAds/ISUnityAdsAdapter/ISUnityAdsAdapter.h
+++ Adapters/UnityAds/ISUnityAdsAdapter/ISUnityAdsAdapter.h
@@ -9,7 +9,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <IronSource/IronSource.h>
 
-static NSString * const UnityAdsAdapterVersion = @"4.3.53";
+static NSString * const UnityAdsAdapterVersion = @"4.3.54";
 static NSString * Githash = @"";
 
 //System Frameworks For UnityAds Adapter
