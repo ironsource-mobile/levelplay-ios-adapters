d1f490477249fb4bfc30a60dc1a26df4139b3d7c
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-08-05 11:16:59 +0000

    Release Bigo Adapter 4.3.8 Supports SDK 4.9.0

--- Adapters/Bigo/ISBigoAdapter/ISBigoAdapter.h
+++ Adapters/Bigo/ISBigoAdapter/ISBigoAdapter.h
@@ -2,14 +2,14 @@
 //  ISBigoAdapter.h
 //  ISBigoAdapter
 //
-//  Copyright © 2024 ironSource Mobile Ltd. All rights reserved.
+//  Copyright © 2021-2025 Unity Technologies. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <IronSource/IronSource.h>
 
-static NSString * const BigoAdapterVersion = @"4.3.7";
+static NSString * const BigoAdapterVersion = @"4.3.8";
 static NSString * Githash = @"";
 
 @interface ISBigoAdapter : ISBaseAdapter
