6c2703e69e4ab624a68a4fd03d6a5e2806a9b123
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-08-11 09:00:04 +0000

    Release BidMachine Adapter 4.3.19 Supports SDK 3.4.0

--- Adapters/BidMachine/ISBidMachineAdapter/ISBidMachineAdapter.h
+++ Adapters/BidMachine/ISBidMachineAdapter/ISBidMachineAdapter.h
@@ -7,7 +7,7 @@
 
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const BidMachineAdapterVersion = @"4.3.18";
+static NSString * const BidMachineAdapterVersion = @"4.3.19";
 static NSString * Githash = @"";
 
 //No System Frameworks For BidMachine Adapter are required
