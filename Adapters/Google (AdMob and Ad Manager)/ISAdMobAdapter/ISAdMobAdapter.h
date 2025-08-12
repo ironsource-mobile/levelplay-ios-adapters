6f35b735101a3645752b9d84e18598014174b9eb
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-08-12 12:26:23 +0000

    Release AdMob Adapter 4.3.70 Supports SDK 12.9.0

--- Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/ISAdMobAdapter.h
+++ Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/ISAdMobAdapter.h
@@ -9,7 +9,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
-static NSString * const AdMobAdapterVersion = @"4.3.69";
+static NSString * const AdMobAdapterVersion = @"4.3.70";
 static NSString * Githash = @"";
 
 //System Frameworks For AdMob Adapter
