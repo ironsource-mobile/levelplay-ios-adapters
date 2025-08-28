55f8e3e1f443b3a57058a78a1c99951f3974aad9
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-08-28 12:40:49 +0000

    Release Pangle Adapter 4.3.54 Supports SDK 7.5.0.7

--- Adapters/Pangle/ISPangleAdapter/ISPangleAdapter.h
+++ Adapters/Pangle/ISPangleAdapter/ISPangleAdapter.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const PangleAdapterVersion = @"4.3.53";
+static NSString * const PangleAdapterVersion = @"4.3.54";
 static NSString * Githash = @"";
 
 // System Frameworks For Pangle Adapter
