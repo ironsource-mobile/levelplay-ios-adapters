f886b81ed6a3ef696f98006ed0dcc609eb321139
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-09-03 13:56:34 +0000

    Release Chartboost Adapter 4.3.25 Supports SDK 9.9.3

--- Adapters/Chartboost/ISChartboostAdapter/ISChartboostAdapter.h
+++ Adapters/Chartboost/ISChartboostAdapter/ISChartboostAdapter.h
@@ -9,7 +9,7 @@
 #import <IronSource/ISBaseAdapter+Internal.h>
 #import <IronSource/IronSource.h>
 
-static NSString * const ChartboostAdapterVersion = @"4.3.24";
+static NSString * const ChartboostAdapterVersion = @"4.3.25";
 static NSString * Githash = @"";
 
 //System Frameworks For Chartboost Adapter
