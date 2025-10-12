f56c082287c49e02de54c61ad83369027bf91120
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2025-10-12 10:42:52 +0000

    Release Verve Adapter 5.1.0 Supports SDK 3.7.0

--- Adapters/Verve/ISVerveAdapter/ISVerveAdapter.h
+++ Adapters/Verve/ISVerveAdapter/ISVerveAdapter.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <IronSource/ISBaseAdapter+Internal.h>
 
-static NSString * const VerveAdapterVersion = @"5.0.0";
+static NSString * const VerveAdapterVersion = @"5.1.0";
 static NSString * Githash = @"";
 
 //System Frameworks For VerveAdapter
