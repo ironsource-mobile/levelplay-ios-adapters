51867b549d726bfec6eaf54887bd05f5dab69091
Smira Ibrahim Rassas <samira.ibrahimrassas@unity3d.com>
2025-12-14 14:37:02 +0200

    Revert "Release Moloco Adapter 4.3.27 Supports SDK 4.1.2"
    This reverts commit 21b7b51bcc4d0fe20b40cf7be1526151635cb235.

--- Adapters/Moloco/ISMolocoAdapter/Interstitial/ISMolocoInterstitialAdapter.m
+++ Adapters/Moloco/ISMolocoAdapter/Interstitial/ISMolocoInterstitialAdapter.m
@@ -150,7 +150,7 @@
 
 #pragma mark - Memory Handling
 
-- (void)releaseMemoryWithAdapterConfig:(ISAdapterConfig *)adapterConfig {
+- (void)destroyInterstitialAdWithAdapterConfig:(ISAdapterConfig *)adapterConfig {
     NSString *adUnitId = [self getStringValueFromAdapterConfig:adapterConfig
                                                         forKey:kAdUnitId];
     LogAdapterDelegate_Internal(@"adUnitId = %@", adUnitId);
